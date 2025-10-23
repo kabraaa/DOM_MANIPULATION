7b3f72afff1f8e8c48cc1add6d8c62e927d3e3fb
Adrian Cano UDIT <adrian.cano@udit.es>
2025-10-23 15:25:42 +0200

    menu responisve

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Hola");
+// Lógica del menú
+
+// Selección de elementos del HTML
+let burger = document.querySelector(".burger > i");
+let menu_opt = document.querySelector(".menu");
+
+// Variable para saber si el menu esta abierto o cerrado
+let isOpen = false;
+
+// Definimos evento click sobre el boton del menú
+burger.addEventListener("click", function(){
+    
+    if (isOpen === false) {
+
+        menu_opt.style.right = "0";
+        isOpen = true;
+
+    } else {
+
+        menu_opt.style.right = "-100%";
+        isOpen = false;
+
+    }
+    
+});
